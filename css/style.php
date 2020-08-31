cb0738f0d5665f2e4c18518dfee799531966c09b
poko1 <42430946+poko1@users.noreply.github.com>
2020-08-31 20:45:01 +0600

    Update style.php

--- css/style.php
+++ css/style.php
@@ -1,9 +1,6 @@
 <style type="text/css">
 
-*{ margin: 0; padding: 0; box-sizing: border-box; }
+*{ margin-left: 2px; margin-right: 2px; padding: 0; box-sizing: border-box; }
 
-#tbval{
-	table-layout: fixed;
-	width=100%;
-}
+
 </style>
